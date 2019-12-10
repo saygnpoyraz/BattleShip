a373fdf18b38de7542e5848bccd1b0509d507e00
iremdanisman <iremdanisman@gmail.com>
2019-12-10 19:32:52 +0300

    Added Iterator Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args){
-
-        GameManager gameManager = new GameManager();
-        gameManager.createBoard();
-        gameManager.printBoard();
-
-        while(gameManager.getBoard().getPlayer1Iterator().hasNext()){
-            System.out.println(gameManager.getBoard().getPlayer1Iterator().next().getName());
-        }
-
-
+        Facade facade = new Facade();
+        facade.createGame();
+        facade.startGame();
     }
 }
