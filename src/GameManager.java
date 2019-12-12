3b5eb67acc6f61afa3f454ba3a409c0721531946
iremdanisman <iremdanisman@gmail.com>
2019-12-12 15:32:07 +0300

    Player class added and code refactored

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,101 +1,152 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class GameManager {
-    private Board board;
-    private ShipIterator player1Iterator;
-    private ShipIterator player2Iterator;
+//    private Board board;
+//    private ShipIterator player1Iterator;
+//    private ShipIterator player2Iterator;
+    int turn = 1;
+    Player player1;
+    Player player2;
+    Player currentPlayer;
+    int selectedShipIndex;
+    Scanner scanner;
 
 
     public GameManager(){
-
+        player1 = new Player(1);
+        player2 = new Player(2);
+        currentPlayer = player1;
+        scanner = new Scanner(System.in);
     }
 
-    public void createBoard(){
-        board = new Board();
+    public int getTurn(){
+        return turn;
     }
+    // public void createBoard(){
+//        board = new Board();
+//    }
 
-    public void printBoard(int player){
-
-        board.printBoard(player);
-
-    }
-
-
-    public Board getBoard() {
-        return board;
-    }
-
-    public void setVariables(){
-        player1Iterator = board.getPlayer1Iterator();
-        player2Iterator = board.getPlayer2Iterator();
-    }
-
-    public void printPlayer1Ship(){
-        while(player1Iterator.hasNext()){
-            System.out.println(player1Iterator.getIndex() + ": " + player1Iterator.next().getName());
+    public boolean isGameOver(){
+        boolean gameOver = false;
+        if (player1.isGameOver()) {
+            player2.gameWon();
+            gameOver = true;
         }
-        player1Iterator.reset();
-    }
-    public void printPlayer2Ship(){
-        while(player2Iterator.hasNext()){
-            System.out.println(player2Iterator.getIndex() + ": " + player2Iterator.next().getName());
+        if (player2.isGameOver()){
+            player1.gameWon();
+            gameOver = true;
         }
-        player2Iterator.reset();
+
+        return gameOver;
     }
 
-    public int isGameOver(){
-        int result = 0;
-
-        int player1ShipCount = player1Iterator.getShipStore().getSize();
-        while(player1Iterator.hasNext()){
-            if(player1Iterator.next().isDead()){
-                player1ShipCount--;
+    public void getShipInput(){
+        boolean checkInput = true;
+        while(checkInput){
+            currentPlayer.printShipMenu();
+            System.out.println("Choose Your Ship To Get Action");
+            try {
+                selectedShipIndex = scanner.nextInt();
+                if (selectedShipIndex < currentPlayer.getShipCount() && selectedShipIndex > -1)checkInput = false;
+                else System.out.println("There is no ship in that index");
+            }catch (InputMismatchException e){
+                scanner = new Scanner(System.in);
+                System.out.println("Invalid input please enter a number");
             }
         }
-        player1Iterator.reset();
-
-        int player2ShipCount = player2Iterator.getShipStore().getSize();
-        while(player2Iterator.hasNext()){
-            if(player2Iterator.next().isDead()){
-                System.out.println(player2Iterator.getIndex());
-                player2ShipCount--;
-            }
-        }
-        player2Iterator.reset();
-
-
-        if (player1ShipCount == 0) result = 1;
-        if (player2ShipCount == 0) result = 2;
-
-        return result;
     }
+
+    public void chooseAction(){
+        char action;
+        do {
+            System.out.println("Choose Your Action\n a for Attack \n d for Defence");
+            action = scanner.next().charAt(0);
+        }while(!(action == 'a' || action == 'd'));
+
+        if (action == 'a'){
+            int x;
+            int y;
+            System.out.println("Enter The Position You Want To Attack ");
+            System.out.print("X: ");
+            x = scanner.nextInt();
+            System.out.print("Y: ");
+            y = scanner.nextInt();
+            currentPlayer.getShip(selectedShipIndex).attack(returnEnemyPos(x,y));
+
+        }
+        if (action == 'd'){
+            currentPlayer.getShip(selectedShipIndex).defence();
+        }
+    }
+
+    public void switchTurn(){
+        turn++;
+        if (turn % 2 == 0) currentPlayer = player2;
+        else currentPlayer = player1;
+    }
+
+    public void printBoard(){
+        currentPlayer.printBoard();
+    }
+
+
+    public Position returnEnemyPos(int x,int y){
+        if (currentPlayer.getId() == 1) return player2.getPosition(x,y);
+        return player1.getPosition(x, y);
+    }
+
+//
+//    public Board getBoard() {
+//        return board;
+//    }
+//
+//    public void setVariables(){
+//        player1Iterator = board.getPlayer1Iterator();
+//        player2Iterator = board.getPlayer2Iterator();
+//    }
+
+    public void printShips(){
+        currentPlayer.printShipMenu();
+    }
+
+//    public void printPlayer1Ship(){
+//        while(player1Iterator.hasNext()){
+//            System.out.println(player1Iterator.getIndex() + ": " + player1Iterator.next().getName());
+//        }
+//        player1Iterator.reset();
+//    }
+//    public void printPlayer2Ship(){
+//        while(player2Iterator.hasNext()){
+//            System.out.println(player2Iterator.getIndex() + ": " + player2Iterator.next().getName());
+//        }
+//        player2Iterator.reset();
+//    }
+
 
     public void updateShips()
     {
-        while(player1Iterator.hasNext()){
-            if(player1Iterator.next().isDead()){
-                player1Iterator.removeShip(player1Iterator.getIndex()-1);
-            }
-        }
-        player1Iterator.reset();
-
-        while(player2Iterator.hasNext()){
-            if(player2Iterator.next().isDead()){
-                player2Iterator.removeShip(player2Iterator.getIndex()-1);
-            }
-        }
-        player2Iterator.reset();
+        if (turn % 2 == 0) player1.updateShips();
+        else player2.updateShips();
     }
 
 
-    public int shipCount(int player){
-        if (player == 1) return player1Iterator.getShipStore().getSize();
-        return player2Iterator.getShipStore().getSize();
-    }
+//    public int shipCount(int player){
+//        if (player == 1) return player1Iterator.getShipStore().getSize();
+//        return player2Iterator.getShipStore().getSize();
+//
+//    }
+//
+//    public void getPlayerShip(){
+//
+//    }
 
-    public Ship getPlayer1Ship(int index){
-        return player1Iterator.getShipStore().getCharAt(index);
-    }
-    public Ship getPlayer2Ship(int index){
-        return player2Iterator.getShipStore().getCharAt(index);
-    }
+//
+//    public Ship getPlayer1Ship(int index){
+//        return player1Iterator.getShipStore().getCharAt(index);
+//    }
+//    public Ship getPlayer2Ship(int index){
+//        return player2Iterator.getShipStore().getCharAt(index);
+//    }
 
 }
